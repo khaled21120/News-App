cd29b6402e37674feb276b88110e9af7988b6ed6
khaled21120 <wwehor@gmail.com>
2025-05-15 07:59:53 +0300

    Finish The App

--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -1,11 +1,28 @@
 import 'package:dio/dio.dart';
 
 import '../errors/error.dart';
+import '../utils/endpoints.dart';
 
 class ApiService {
   final Dio dio;
 
-  ApiService({required this.dio});
+  ApiService()
+    : dio = Dio(
+        BaseOptions(
+          baseUrl: Endpoints.baseUrl,
+          connectTimeout: const Duration(seconds: 20),
+          receiveTimeout: const Duration(seconds: 20),
+        ),
+      ) {
+    dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) {
+          options.headers['x-api-key'] = 'cbcf2a0dac044a158e1b7f494629951b';
+          return handler.next(options);
+        },
+      ),
+    );
+  }
   Future<Map<String, dynamic>> getMapData({required String endPoint}) async {
     try {
       final res = await dio.get(endPoint);
@@ -18,5 +35,4 @@ class ApiService {
       throw ServerFailure.fromDioException(dioError);
     }
   }
-
 }
--- lib/core/services/get_it_service.dart
+++ lib/core/services/get_it_service.dart
@@ -1,28 +1,14 @@
-import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:news/core/services/api_service.dart';
 import 'package:news/features/home/cubit/news/news_cubit.dart';
 
 import '../../features/home/data/repo/news_repo.dart';
 import '../../features/home/data/repo/news_repo_impl.dart';
-import '../utils/endpoints.dart';
 
 final getIt = GetIt.instance;
 
 void setupServiceLocator() {
-  getIt.registerSingleton<Dio>(
-    Dio(
-      BaseOptions(
-        baseUrl: Endpoints.baseUrl,
-        connectTimeout: const Duration(seconds: 20),
-        receiveTimeout: const Duration(seconds: 20),
-        headers: {
-          'x-api-key': 'cbcf2a0dac044a158e1b7f494629951b',
-        },
-      ),
-    ),
-  );
-  getIt.registerSingleton<ApiService>(ApiService(dio: getIt.get<Dio>()));
+  getIt.registerSingleton<ApiService>(ApiService());
   getIt.registerSingleton<NewsRepo>(NewsRepoImpl(getIt.get<ApiService>()));
   getIt.registerFactory<NewsCubit>(() => NewsCubit(getIt.get<NewsRepo>()));
 }
